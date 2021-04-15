9e16a45a97decbad11118ccb8c4034df69d57591
Jakub Kozlik <jakubkozlik@Jakubs-MacBook-Pro.local>
2021-04-15 15:59:55 +0200

    Usunąłem funkcję number_check() i zawarłem jej funckjonalność w funkcji number_input(), której nazwę zmieniłem na guess_number().

--- main.py
+++ main.py
@@ -36,13 +36,21 @@ random_num = randint(1, 101)
 user_num = 0
 
 
-def number_input():
+def guess_number():
     while True:
         try:
             user_num = int(input("Guess the number: "))
             if user_num in range(1, 101):
-                return user_num
-                # exit()
+                print(f"The number you declared is: {user_num}.")
+                if user_num == random_num:
+                    print("You win!")
+                    return user_num
+                elif user_num > random_num:
+                    print("To big!")
+                    continue
+                elif user_num < random_num:
+                    print("Too small!")
+                    continue
             else:
                 print("Number should be between 1 and 100")
                 continue
@@ -51,9 +59,5 @@ def number_input():
             print("It's not a number!")
 
 
-def number_check():
-    print(f"The number you declared is: {number_input()}.")
-
-
-number_check()
+guess_number()
 
