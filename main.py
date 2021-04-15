64b3622ae04b7c36a8ca404051bc7b1a8f894bea
Jakub Kozlik <jakubkozlik@Jakubs-MacBook-Pro.local>
2021-04-15 16:00:56 +0200

    Uprościłem kod i oczyściłem z niepotrzebnych linijek kodu.

--- main.py
+++ main.py
@@ -1,35 +1,3 @@
-# from random import randint
-
-# class GuessingGame:
-#     def __init__(self, random_num, user_num):
-#         self.random_num = random_num
-#         self.user_num = user_num
-#         random_num = randint(1, 101)
-#         user_num = 0
-#
-#
-#     def number_input():
-#         while True:
-#             try:
-#                 self.user_num
-#                 user_num = int(input("Guess the number: "))
-#                 if user_num in range(1, 101):
-#                     exit()
-#                 else:
-#                     print("Number should be between 1 and 100")
-#                     continue
-#                 exit()
-#             except (ValueError, TypeError):
-#                 print("It's not a number!")
-#
-#
-#     def number_check(self, user_num):
-#         print(f"The number you declared is: {user_num}.")
-#
-#
-#     number_input()
-#     number_check()
-
 from random import randint
 
 random_num = randint(1, 101)
